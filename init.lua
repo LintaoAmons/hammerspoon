61cccaf9eb77490ce85357a492913eb8f8346c6a
Lintao <edwardlin.zlt@gmail.com>
2022-04-04 11:19:27 +0800

    add scenario shortcuts

--- init.lua
+++ init.lua
@@ -1,9 +1,7 @@
--- 
 require('windows')
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
-hs.alert.show("Hammerspoon config reloaded")
 
 -- hs.hotkey.bind(hyperKey, 'a', winwin:moveAndResize("halfleft"))
 -- half of screen
@@ -28,17 +26,32 @@ local function remap(mods, key, pressFn)
 	return hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
 end
 
+
 local scenarioShortcuts = {
-  nextTab = nil,
-  prevTab = nil
+  firefox = {
+    nextTab = remap({'cmd', 'ctrl'}, 'l', pressFn({'ctrl'}, 'tab')),
+    prevTab = remap({'cmd', 'ctrl'}, 'h', pressFn({'ctrl', 'shift'}, 'tab'))
+  },
+  test = {
+    testMap = remap({'cmd', 'ctrl'}, 'l', pressFn({'ctrl'}, 'l'))
+  }
 }
 
+local function enableScenarioShortcuts(scenario)
+  for _, value in pairs(scenarioShortcuts[scenario]) do
+    value:enable()
+  end
+end
+
+local function disableScenarioShortcuts(scenario)
+  for _, value in pairs(scenarioShortcuts[scenario]) do
+    value:disable()
+  end
+  print(serializeTable(scenarioShortcuts))
+end
+
+
 function applicationWatcher(appName, eventType, appObject)
-  -- local function clear()
-  --   for key, value in pairs(scenarioShortcuts) do
-  --     if 
-  --   end
-  -- end
 
     if (eventType == hs.application.watcher.activated) then
         -- 初始化senarioShortcuts
@@ -47,21 +60,54 @@ function applicationWatcher(appName, eventType, appObject)
             appObject:selectMenuItem({"Window", "Bring All to Front"})
         end
         if (appName == "iTerm2") then
-          showFocusAlert("TERMINAL")
+            showFocusAlert("TERMINAL")
+            enableScenarioShortcuts('test')
+            disableScenarioShortcuts('firefox')
         end
         if (appName == "IntelliJ IDEA") then
-          showFocusAlert("IDEA")
+            showFocusAlert("IDEA")
         end
         if (appName == "Firefox") then
-          showFocusAlert("FIREFOX")
-           scenarioShortcuts.nextTab = remap({'cmd', 'ctrl'}, 'l', pressFn({'ctrl'}, 'tab'))
-           scenarioShortcuts.prevTab = remap({'cmd', 'ctrl'}, 'h', pressFn({'ctrl', 'shift'}, 'tab'))
+            showFocusAlert("FIREFOX")
+            enableScenarioShortcuts('firefox')
+            disableScenarioShortcuts('test')
         end
         if (appName == "Joplin") then
-          showFocusAlert("JOPLIN")
+            showFocusAlert("JOPLIN")
         end
     end
+      print('current' .. serializeTable(scenarioShortcuts))
+
 end
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
+
+function serializeTable(val, name, skipnewlines, depth)
+    skipnewlines = skipnewlines or false
+    depth = depth or 0
+
+    local tmp = string.rep(" ", depth)
+
+    if name then tmp = tmp .. name .. " = " end
+
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+
+        for k, v in pairs(val) do
+            tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+        end
+
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+
+    return tmp
+end
