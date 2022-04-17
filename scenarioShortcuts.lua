fbb29ad1753811dd040cba1d560273d9d244dfa9
Lintao <edwardlin.zlt@gmail.com>
2022-04-17 10:58:04 +0800

    enhance tmux-switch-pane

--- scenarioShortcuts.lua
+++ scenarioShortcuts.lua
@@ -1,105 +1,76 @@
-function showFocusAlert(content) 
+function showFocusAlert(content)
     hs.alert.show(content, hs.alert.defaultStyle, hs.screen.mainScreen(), 0.5)
 end
 
 local function keyStroke(mods, key)
-	if key == nil then
-		key = mods
-		mods = {}
-	end
+    if key == nil then
+        key = mods
+        mods = {}
+    end
 
-	return function() hs.eventtap.keyStroke(mods, key, 1000) end
+    return function()
+        hs.eventtap.keyStroke(mods, key, 1000)
+    end
 end
 
 local function remap(mods, key, pressFn)
-	return hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
+    return hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
 end
 
--- local function test()
---   return function ()
---       hs.eventtap.keyStroke({}, ':b', 10)
---     end
--- end
-
--- hs.hotkey.bind({'cmd', 'ctrl'}, 'l', test, nil, test)
-
-function switchPane(key)
-  local paneOption
-
-  if key == 'h' then
-    paneOption = '-L'
-  elseif key == 'j' then
-    paneOption = '-D'
-  elseif key == 'k' then
-    paneOption = '-U'
-  elseif key == 'l' then
-    paneOption = '-R'
-  end
-
-  return remap(
-      {'cmd', 'ctrl'},
-      key,
-      function ()
+function tmuxSwitchPane(key)
+    return remap({'cmd', 'ctrl'}, key, function()
         hs.eventtap.keyStroke({'ctrl'}, 'b', 1000)
-        hs.eventtap.keyStroke({'shift'}, ';', 1000)
-        hs.eventtap.keyStrokes("select-pane ".. paneOption)
-        hs.timer.doAfter(0.1, function ()
-          hs.eventtap.keyStroke({}, 'return', 1000)
+        hs.timer.doAfter(0.1, function() 
+          hs.eventtap.keyStroke({}, key)
         end)
-      end
-    )
+    end)
 end
 
 function terminalCommand(key, cmd)
-  return remap(
-      {'cmd', 'ctrl'},
-      key,
-      function ()
+    return remap({'cmd', 'ctrl'}, key, function()
         hs.eventtap.keyStrokes(cmd)
-        hs.timer.doAfter(0.1, function ()
-          hs.eventtap.keyStroke({}, 'return', 1000)
+        hs.timer.doAfter(0.1, function()
+            hs.eventtap.keyStroke({}, 'return', 1000)
         end)
-      end
-    )
+    end)
 
 end
 
 -- lazygit demo
 
 local scenarioShortcuts = {
-  firefox = {
-    nextTab = remap({'cmd', 'ctrl'}, 'l', keyStroke({'ctrl'}, 'tab')),
-    prevTab = remap({'cmd', 'ctrl'}, 'h', keyStroke({'ctrl', 'shift'}, 'tab'))
-  },
-  tmux = {
-    paneRight = switchPane('l'),
-    paneLeft = switchPane('h'),
-    paneUp = switchPane('k'),
-    paneDown = switchPane('j'),
-  },
-  terminal = {
-    lazygit = terminalCommand('u', 'lazygit'),
-    termscp = terminalCommand('i', 'termscp'),
-    lfcd = terminalCommand('o', 'lfcd'),
-    k9s = terminalCommand('9', 'k9s')
-  }
+    firefox = {
+        nextTab = remap({'cmd', 'ctrl'}, 'l', keyStroke({'ctrl'}, 'tab')),
+        prevTab = remap({'cmd', 'ctrl'}, 'h', keyStroke({'ctrl', 'shift'}, 'tab'))
+    },
+    tmux = {
+        paneRight = tmuxSwitchPane('l'),
+        paneLeft = tmuxSwitchPane('h'),
+        paneUp = tmuxSwitchPane('k'),
+        paneDown = tmuxSwitchPane('j')
+    },
+    terminal = {
+        lazygit = terminalCommand('u', 'lazygit'),
+        termscp = terminalCommand('i', 'termscp'),
+        lfcd = terminalCommand('o', 'lfcd'),
+        k9s = terminalCommand('9', 'k9s')
+    }
 }
 
 -- TODO enable a list and disable others
 
 local function enableScenarioShortcuts(scenario)
-  for _, value in pairs(scenarioShortcuts[scenario]) do
-    value:enable()
-  end
+    for _, value in pairs(scenarioShortcuts[scenario]) do
+        value:enable()
+    end
 end
 
 local function disableScenarioShortcuts(scenario)
-  for _, value in pairs(scenarioShortcuts[scenario]) do
-    value:disable()
-  end
-  print(serializeTable(scenarioShortcuts))
+    for _, value in pairs(scenarioShortcuts[scenario]) do
+        value:disable()
+    end
+    print(serializeTable(scenarioShortcuts))
 end
-
 
 function applicationWatcher(appName, eventType, appObject)
 
@@ -134,12 +105,11 @@ function applicationWatcher(appName, eventType, appObject)
             disableScenarioShortcuts('terminal')
         end
     end
-      print('current' .. serializeTable(scenarioShortcuts))
+    print('current' .. serializeTable(scenarioShortcuts))
 
 end
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
-
 
 function serializeTable(val, name, skipnewlines, depth)
     skipnewlines = skipnewlines or false
@@ -147,13 +117,15 @@ function serializeTable(val, name, skipnewlines, depth)
 
     local tmp = string.rep(" ", depth)
 
-    if name then tmp = tmp .. name .. " = " end
+    if name then
+        tmp = tmp .. name .. " = "
+    end
 
     if type(val) == "table" then
         tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
 
         for k, v in pairs(val) do
-            tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+            tmp = tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
         end
 
         tmp = tmp .. string.rep(" ", depth) .. "}"
